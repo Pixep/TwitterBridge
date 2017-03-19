8cadb82eb29eb00781b8413e7ea8c7d43d860ca1
Adrien Leravat <adrien.leravat@gmail.com>
2017-03-19 12:29:16 +0100

    Improved module organization, and adapted BDD tests

--- libs/timeline.js
+++ libs/timeline.js
@@ -1,29 +1,50 @@
 var Entities = require('html-entities').XmlEntities
 var Twitter = require('twitter')
 
-// Twitter parameters from environment variables
-var twitterAccount = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
+/**
+* @brief Update account information from environment
+*/
+function _twitterFromEnvironment () {
+  _assertEnvironmentSet()
+  return new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
+}
 
-var twitterTimelineParams = {count: 30}
+/**
+* @brief Make sure the appropriate environment variables are set, or throw
+*/
+function _assertEnvironmentSet () {
+  if ( process.env.TWITTER_CONSUMER_KEY == undefined          || process.env.TWITTER_CONSUMER_KEY == null
+    || process.env.TWITTER_CONSUMER_SECRET == undefined       || process.env.TWITTER_CONSUMER_SECRET == null
+    || process.env.TWITTER_ACCESS_TOKEN_KEY == undefined      || process.env.TWITTER_ACCESS_TOKEN_KEY == null
+    || process.env.TWITTER_ACCESS_TOKEN_SECRET == undefined   || process.env.TWITTER_ACCESS_TOKEN_SECRET == null)
+    throw new Error("One or more of the following environment variables is not set: " +
+          "TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET " +
+          "TWITTER_ACCESS_TOKEN_KEY, TWITTER_ACCESS_TOKEN_SECRET");
+}
 
 module.exports = {
+  twitter: _twitterFromEnvironment(),
+  assertEnvironmentSet: _assertEnvironmentSet,
+  twitterFromEnvironment: _twitterFromEnvironment,
+
   /**
-  * @brief Returns true if query is authorized
+  * @brief Returns tweeter timeline
+  * @param callback(error, tweets, response)
   */
-  queryAuthorized: function (req) {
-    return (req.params.pass == "1645328")
-  },
-
   getTimeline: function (callback) {
-    twitterAccount.get('statuses/home_timeline', twitterTimelineParams, function(error, tweets, response) {
-      var timeline = module.exports.parseTwitterTimeline(error, tweets, response)
+    module.exports.assertEnvironmentSet();
 
-      // @todo return errors
+    var twitterTimelineParams = {count: 30}
+    module.exports.twitter.get('statuses/home_timeline', twitterTimelineParams, function(error, tweets, response) {
+      if (error)
+        callback(error, null);
+
+      var timeline = module.exports.parseTwitterTimeline(tweets);
       callback(null, timeline);
     })
   },
@@ -31,14 +52,9 @@ module.exports = {
   /**
   * @brief Parses twiter timeline content
   */
-  parseTwitterTimeline: function (error, tweets, response) {
-    if (error) {
-      console.log('Tweeter timeline error ! %j', error);
-      return;
-    }
-
+  parseTwitterTimeline: function (tweets) {
     for(var i = 0; i < tweets.length; i++)
-      tweets[i] = formatTweet(tweets[i]);
+      tweets[i] = module.exports.formatTweet(tweets[i]);
 
     return tweets;
   },
---
+++ spec/timeline-test.js
@@ -0,0 +1,83 @@
+var Timeline = null;
+var should = require('chai').should();
+var expect = require('chai').expect;
+var sinon = require('sinon');
+
+describe('Timeline module', function () {
+  it('should throw when required, if environment is not set properly', function (done) {
+    expect(function() { require('../libs/timeline') }).to.throw(Error);
+    done();
+  })
+
+  it('should not throw when required, if environment is set properly', function (done) {
+    process.env.TWITTER_CONSUMER_KEY = "key";
+    process.env.TWITTER_CONSUMER_SECRET = "secret";
+    process.env.TWITTER_ACCESS_TOKEN_KEY = "key";
+    process.env.TWITTER_ACCESS_TOKEN_SECRET = "secret";
+
+    expect(function() {Timeline = require('../libs/timeline');}).to.not.throw(Error);
+    done();
+  })
+})
+
+describe('Retrieve tweets timeline', function () {
+  describe('#getTimeline()', function() {
+    it('should return an error if authentication fails', function (done) {
+      Timeline.getTimeline(function(error, tweets) {
+        expect(error).to.exist;
+        expect(tweets).to.be.null;
+        done();
+      });
+    });
+    it('should return tweets when no error occurs', function (done) {
+      var stub = sinon.stub(Timeline.twitter, "get").callsFake(function(path, params, callback) {
+        var tweets = [
+          { text: "tweet 1" },
+          { text: "tweet 2" }
+        ];
+        callback(null, tweets, null);
+      });
+
+      Timeline.getTimeline(function(error, tweets) {
+        expect(error).to.be.null;
+        expect(tweets).to.exist;
+        expect(tweets).to.have.lengthOf(2);
+        done();
+      });
+    });
+  });
+});
+
+describe('Formatting', function () {
+  describe('#formatTweet()', function() {
+    it('should remove double line breaks', function (done) {
+      var testTweet = {
+        text: "My tweet\n\nbreaks\n\nlines !"
+      }
+
+      testTweet = Timeline.formatTweet(testTweet);
+      testTweet.text.should.equal("My tweet\nbreaks\nlines !");
+      done();
+    });
+
+    it('should remove trailing tweeter url', function (done) {
+      var testTweet = {
+        text: "My tweet https://t.co/iHu2eg8h"
+      }
+
+      testTweet = Timeline.formatTweet(testTweet);
+      testTweet.text.should.equal("My tweet ");
+      done();
+    });
+
+    it('should decode HTML entities', function (done) {
+      var testTweet = {
+        text: "My &amp;&#212; &#174;"
+      }
+
+      testTweet = Timeline.formatTweet(testTweet);
+      testTweet.text.should.equal("My &Ô ®");
+      done();
+    });
+  });
+});
